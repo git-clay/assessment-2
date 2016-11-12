3572aeafe241d4a314120512ea11685be620765f
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-12 11:58:23 -0700

    index looks solid

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 //Your game page must have at least two player objects
 //Your game must have a clear win condition
+function makePlayer(){
+	
+}
+
+function onBoard(element, location){		//element === object and location is where it is placed in html
+
+}
+
+
+var player1 = new makePlayer();
+var player2 = new makePlayer();
