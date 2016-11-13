d17808f171687efc3f9454dade7a19ae93848ca1
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-13 08:22:57 -0700

    solved the div.id problem

--- main.js
+++ main.js
@@ -2,19 +2,18 @@
 //Your game must have a clear win condition
 var start = 50;
 var end = 550;
-var body = document.querySelector('body');
-var article = document.querySelector('article');
-
+//var body = document.querySelector('body');
+var section = document.querySelector('section');
 
 /*----------------create article/section/aside for divs to go into??--------------------*/
 
 
 function Player(tag,y,position,id){
 	this.tag= document.createElement(tag);		//makes div
-	this.tag.innerHTML = "Player "+id;			//places "player" into div tag
-	body.append(this.tag);						//div into body
+	this.tag.innerHTML = id;			//places "player" into div tag
+	section.append(this.tag);						//div into body
 	this.y=y;
-	this.id= id;
+	this.tag.id= id;
 	this.position=position;
 	this.class='player';
 }
@@ -27,13 +26,14 @@ console.log(element.tag);
 
 }
 
-var player1 = new Player("div",100,start,"1");
-var player2 = new Player("div",300,start,"2");
+var player1 = new Player("div",100,start,"player1");
+var player2 = new Player("div",300,start,"player2");
+//player1.setAttribute('id', 'player1');
 //onBoard(player1,body);
 
 /* Create key pressed functions for each player*/
 function zPressed(position){
-	var element = document.getElementById("1");
+	var element = document.getElementById("Player1");
 	element.style.position=parseInt(element.style.position) +5 +'px';
 }
 
