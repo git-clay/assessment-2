6f4e55547535b9bd7a5a54721ad472cc4b0637ea
Clayton Smith <claybin@claytons-MacBook-Pro.local>
2016-11-13 11:42:24 -0700

    players move!

--- main.js
+++ main.js
@@ -1,45 +1,58 @@
 //Your game page must have at least two player objects
 //Your game must have a clear win condition
-var start = 50;
-var end = 550;
+var start = 0;
+var end = 350;
 //var body = document.querySelector('body');
 var section = document.querySelector('section');
 var win = false;
 
 /*----------------create article/section/aside for divs to go into??--------------------*/
-
-
 function Player(tag,positionY,positionX,id){
 	this.tag= document.createElement(tag);		//makes div
 	this.tag.innerHTML = id;					//Writes name onto character
 	section.append(this.tag);					//div into body
 	this.positionY=positionY;									//vertical position on section
 	this.tag.id= id;							//places id into div tag
-	this.positionX=positionX;						//current x position (start>>>end)
+	this.positionX = positionX;						//current x position (start>>>end)
+	onBoard(this);								//work on streamlining....
 }
+var player1 = new Player("div",2,start,"player1");
+var player2 = new Player("div",24,start,"player2");
+
+function onBoard(player){
+	var currentID = document.getElementById(player.tag.id);
+	currentID.setAttribute("style","left:"+player.positionX+"px"); //changes X position!
+	currentID.setAttribute("style","top:"+player.positionY+"%"); //changes y position!
+//console.log(currentID);
+}
+
+/*________________________Functions to move players_________________*/
 Player.prototype.draw = function(object){
 	object.fillRect(this.positionX,this.positionY, 75,75);
 };
-Player.prototype.move =function(){
-	this.positionX ++;
-	console.log('moved');
+Player.prototype.move =function(player){				///function to moves player
+	var currentID = document.getElementById(player.tag.id);
+	console.log(player.positionX);
+	currentID.setAttribute("style","left:"+player.positionX+"px"); //changes X position!
+	player.positionX +=15;
+
 };
-document.addEventListener('keydown', function(event){
+document.addEventListener('keydown', function(event){		//calls move function when key is pressed
 	switch(event.keyCode){
 		case 90: //z
-			player1.move();
-			console.log('z');
+			player1.move(player1);
+				console.log("player 1 moved to "+player1.positionX);
+
 		break;
 		case 77: //m
-			player2.move();
+			player2.move(player2);
+				console.log("player 2 moved to "+player2.positionX);
 		break;
 	}
 });
 
-var player1 = new Player("div",100,start,"player1");
-var player2 = new Player("div",300,start,"player2");
+
 //player1.draw();
-//player1.move();
 //console.log(player1); 
 
 
@@ -47,6 +60,14 @@ var player2 = new Player("div",300,start,"player2");
 /*
 while (win===false){
 	document.addEventListener('keyCode=122',hey);
+	if (player1.positionX >end){
+	alert('Player 1 wins!');
+	break;
+	}
+	else if(player2.positionX >end){
+	alert('Player 2 wins!');
+	break;
+	}
 }
 */
 /*Reset button*/
